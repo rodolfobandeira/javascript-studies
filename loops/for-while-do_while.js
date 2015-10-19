c5c0ce773ef00e3c4abfcf211c40e600efe24c98
Rodolfo Bandeira <rbandeira@demacmedia.com>
2015-10-19 16:12:42 -0400

    for, while and do while

---
+++ loops/for-while-do_while.js
@@ -0,0 +1,17 @@
+for (var x = 0; x < 3; x++){
+    console.log("for " + x);
+}
+
+var y = 0;
+while(y < 3){
+    console.log("while " + y);
+    y++;
+}
+
+var z = 0;
+do {
+    console.log("do while " + z);
+    z++;
+} while (z < 3);
+
+
