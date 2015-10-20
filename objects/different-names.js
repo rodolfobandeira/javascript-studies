1015b8c1c731c48442d00f6f89b9f55c11ddca7a
rodolfobandeira <contato@rodolfobandeira.com>
2015-10-20 08:28:05 -0400

    objects

---
+++ objects/different-names.js
@@ -0,0 +1,11 @@
+// This is LITERAL NOTATION
+var snoopy = {
+    species: "beagle",
+    age: 10
+};
+
+// This is CONSTRUCTOR NOTATION
+var buddy = new Object();
+buddy.species = 'golden retriever';
+buddy.age = 5;
+// FILLED BY DOT NOTATION
