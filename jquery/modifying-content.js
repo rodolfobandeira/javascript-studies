981634c7b91e0db65d9b71e34f7224776434ebd7
rodolfobandeira <contato@rodolfobandeira.com>
2015-10-24 08:26:57 -0400

    jquery-and-css-tests

---
+++ jquery/modifying-content.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('div').height('200px');
+    $('div').width('200px');
+    $('div').css('border-radius', '10px');
+});
+
---
+++ jquery/toggleClass-example.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#text').click(function (){
+        $(this).toggleClass('highlighted');    
+    });    
+});
+
+/*
+
+#text {
+    background-color: #0B486B;
+    border-radius: 5px;
+    text-align: center;
+    font-family: Vivaldi, Cursive;
+    color: #FFFFFF;
+    width: 200px;
+    height: 25px;
+}
+
+.highlighted {
+    -webkit-box-shadow: 0 0 8px #FFD700;
+    -moz-box-shadow: 0 0 8px #FFD700;
+    box-shadow: 0 0 8px #FFD700;
+    cursor:pointer;
+}
+
+*/
+
